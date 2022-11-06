1be1424515ed067a4f3b6f324ca89ca02e5b6aee
Philipp Böschen <catouc@philipp.boeschen.me>
2022-11-06 19:52:51 +0100

    Update my flake with my new standard

--- flake.nix
+++ flake.nix
@@ -1,48 +1,48 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
   };
 
   description = "gitlab-ci-crawler, building a dependency graph for CI includes";
 
   outputs = { self, nixpkgs, flake-utils }:
   flake-utils.lib.eachDefaultSystem (system:
-  let
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-    rec {
-      packages = flake-utils.lib.flattenTree {
-        gitlabCICrawler = pkgs.buildGoModule {
-          pname = "gitalb-ci-crawler";
-          version = "v0.13.6";
-          modSha256 = pkgs.lib.fakeSha256;
-          vendorSha256 = null;
-          src = ./.;
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      build = pkgs.buildGoModule {
+        pname = "gitalb-ci-crawler";
+        version = "v0.13.6";
+        modSha256 = pkgs.lib.fakeSha256;
+        vendorSha256 = null;
+        src = ./.;
 
-          meta = {
-            description = "gitlab-ci-crawler, building a dependency graph for CI includes";
-            homepage = "https://github.com/catouc/gitlab-ci-crawler";
-            license = pkgs.lib.licenses.mit;
-            maintainers = [ "catouc" ];
-            platforms = pkgs.lib.platforms.linux;
-          };
+        meta = {
+          description = "gitlab-ci-crawler, building a dependency graph for CI includes";
+          homepage = "https://github.com/catouc/gitlab-ci-crawler";
+          license = pkgs.lib.licenses.mit;
+          maintainers = [ "catouc" ];
+          platforms = pkgs.lib.platforms.linux ++ pkgs.lib.platforms.darwin;
         };
       };
+  in
+    rec {
+      packages = {
+        gitlab-ci-crawler = build;
+        default = build;
+      };
 
-      defaultPackage = packages.gitlabCICrawler;
-      defaultApp = packages.gitlabCICrawler;
-
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.docker
-          pkgs.gcc
-          pkgs.go
-        ];
-        shellHook = ''
-          export NEO4J_USERNAME='neo4j'
-          export NEO4J_PASSWORD='neo4j'
-        '';
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.docker
+            pkgs.go
+          ];
+          shellHook = ''
+            export NEO4J_USERNAME='neo4j'
+            export NEO4J_PASSWORD='neo4j'
+          '';
+        };
       };
     }
   );
