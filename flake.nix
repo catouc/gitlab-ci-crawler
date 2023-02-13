aaf6e1caeb3aa79ceb18ecc45409d9c60c94856a
Tobias Germer <tobias.germer@tui.com>
2023-02-13 11:33:17 +0100

    bump version

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       build = pkgs.buildGoModule {
         pname = "gitalb-ci-crawler";
-        version = "v0.13.8";
+        version = "v0.13.9";
         modSha256 = pkgs.lib.fakeSha256;
         vendorSha256 = null;
         src = ./.;
